7d27d5f426bdd2b7a3381c8211298118e5aa428f
Pedro Leitão <phf.leitao@gmail.com>
2023-03-29 22:45:57 +0100

    Exibindo Dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    public todos: any[] = [];
+    public title: string = 'Minhas Tarefas';
+
+    constructor() {
+        this.todos.push('passear com o cachorro');
+        this.todos.push('receber aumento');
+        this.todos.push('abrir conta');
+    }
 }
